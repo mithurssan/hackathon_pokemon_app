270d2eb1fe2cb9f9514e1676d43247e123f6d5b1
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-25 14:51:15 +0100

    add more stats to pokemon card, update css

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -8,9 +8,15 @@ const PokemonCard = ({ pokemon }) => {
             <div className="pokemon-card">
                 <h2>{pokemon.name}</h2>
                 <img src={pokemon.sprites["front_default"]} alt={pokemon.name} />
-                <ul>Abilities:
+                <p className="physicals">Weight: {pokemon.weight} , Height: {pokemon.height}</p>
+                <ul className="abilities">Abilities:
                     {pokemon.abilities.map(
                         abilities => <li key={abilities.ability.name}>{abilities.ability.name}</li>
+                    )}
+                </ul>
+                <ul className="types">Types:
+                    {pokemon.types.map(
+                        types => <li key={types.type.name}>{types.type.name}</li>
                     )}
                 </ul>
             </div>
