1817730eae73b531026b49121e1289e7920f0d10
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-25 15:16:09 +0100

    add css for list of pokemon

--- src/components/PokemonGallery/index.jsx
+++ src/components/PokemonGallery/index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import PokemonItem from "../PokemonItem"
+import "./style.css"
 
 const PokemonGallery = ({ pokemons }) => {
     return (
         <div className="pokemon">
+            <h1>List of 100 Pokemons!</h1>
             {
                 pokemons.map(pokemon => <PokemonItem key={pokemon.name} pokemon={pokemon} />)
             }
